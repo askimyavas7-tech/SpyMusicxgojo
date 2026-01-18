ff8e44a1b5e71083a301d4bfa71fd1c7e8c98759
ARJUN <pachangedinanath@gmail.com>
2026-01-18 23:58:25 +0530

    Update admin.py

--- Spy/plugins/extra/admin.py
+++ Spy/plugins/extra/admin.py
@@ -32,7 +32,7 @@ kickpic = [
 button = [
     [
         InlineKeyboardButton(
-            text="Ɗᴇᴠ𝘴", url="https://t.me/eternitylover121"
+            text="🍂 Ɗᴇᴠ𝘴", url="https://t.me/uchiha_owner"
         )
     ]
 ]
