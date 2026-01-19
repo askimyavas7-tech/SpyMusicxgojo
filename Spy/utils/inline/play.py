77c8cb2f35ae3076f9d74718181def5ba9927dc0
askimyavas7-tech <askimyavas7@gmail.com>
2026-01-19 20:22:32 +0300

    Update play.py

--- Spy/utils/inline/play.py
+++ Spy/utils/inline/play.py
@@ -18,8 +18,8 @@ def track_markup(_, videoid, user_id, channel, fplay):
             ),
         ],
         [
-            InlineKeyboardButton("📥ᴘʀᴏᴍᴏ📥", url="https://t.me/snowy_hometown"),
-            InlineKeyboardButton("💗ɢʀᴏᴜᴘ ᴄʜᴀᴛ💗", url="https://t.me/destiny_infinity_og"),
+            InlineKeyboardButton("📥ᴘʀᴏᴍᴏ📥", url="https://t.me/kral_surucu"),
+            InlineKeyboardButton("💗ɢʀᴏᴜᴘ ᴄʜᴀᴛ💗", url="https://t.me/yasamakbuysa"),
         ],
         [
             InlineKeyboardButton(
